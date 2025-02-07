e7e25ac04808047c078699e7bdab1546ec186847
18323 <1832386780@qq.com>
2025-02-07 10:12:17 +0800

    2

--- case/shop/demo.py
+++ case/shop/demo.py
@@ -9,6 +9,7 @@ def add(a, b):
     @apiParam {Number} b 入参数字2
     @apiSuccess {Integer} data 返回结果
     @apiSucess(200){Integer} code服务器码
+
     """
     sum = a + b
     return dict(code=0, data=sum)
