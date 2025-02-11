a1956eaf86bf0b54e9a2afc2161facfe8653e3c4
18323 <1832386780@qq.com>
2025-02-11 17:23:40 +0800

    2

--- case/shop/demo.py
+++ case/shop/demo.py
@@ -1,18 +1,5 @@
 
-# def add(a, b):
-#     """
-#
-#     @api{Post}/add 两数加法运算
-#     @apiGroup 业务线
-#     @apiParam {Number} a 入参数字1
-#
-#     @apiParam {Number} b 入参数字2
-#     @apiSuccess {Integer} data 返回结果
-#     @apiSucess(200){Integer} code服务器码
-#
-#     """
-#     sum = a + b
-#     return dict(code=0, data=sum)
+
 def test():
     """
     @api {GET} /test 测试接口
