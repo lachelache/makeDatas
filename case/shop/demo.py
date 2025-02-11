8f9c0fd9a6ca3de473d71c200887806e530067bd
18323 <1832386780@qq.com>
2025-02-11 16:47:52 +0800

    2

--- case/shop/demo.py
+++ case/shop/demo.py
@@ -13,47 +13,8 @@
 #     """
 #     sum = a + b
 #     return dict(code=0, data=sum)
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/add', methods=['POST'])
-def add_numbers():
-    """
-    @api {post} /add 两数相加
-    @apiName AddNumbers
-    @apiGroup Math
-
-    @apiParam {Number} num1 第一个数字
-    @apiParam {Number} num2 第二个数字
-
-    @apiSuccess {Number} result 计算结果
-
-    @apiSuccessExample {json} 成功响应示例:
-        HTTP/1.1 200 OK
-        {
-          "result": 5
-        }
-
-    @apiError BadRequest 参数错误
-
-    @apiErrorExample {json} 错误响应示例:
-        HTTP/1.1 400 Bad Request
-        {
-          "error": "Invalid parameters"
-        }
-    """
-    data = request.get_json()
-
-    num1 = data.get('num1')
-    num2 = data.get('num2')
-
-    if not isinstance(num1, (int, float)) or not isinstance(num2, (int, float)):
-        return jsonify({"error": "Invalid parameters"}), 400
-
-    result = num1 + num2
-    return jsonify({"result": result})
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+"""
+@api {get} /test 测试接口
+@apiName Test
+@apiGroup Debug
+"""
